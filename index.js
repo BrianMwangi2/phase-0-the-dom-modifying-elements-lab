b2e1d7da0c4301263e3c6c9d328bda42e32f326b
Brian <brian.mwangi2@student.moringaschool.com>
2024-04-01 00:46:01 +0300

    adds files

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+// remove main id 
+main.remove();
+
+// have a new header
+   
+const newHeader=document.createElement(`h1`);
+document.body.appendChild(newHeader);
+newHeader.id=`victory`;
+newHeader.textContent="Brian is the champion";
+
